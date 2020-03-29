d35c91ef73b3be546806ff2bb6ab23fdd6ef44c3
Bismantaka Aldila <bismantaka20@hotmail.com>
2020-03-30 04:27:29 +0700

    Refactoring + Compressing

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var time = new Date()
+var androbar = document.getElementById('androbar')
+var grettings = document.getElementById("grettings")
+var body = document.body
+var jam = time.getHours()
+var quote = document.getElementById('quote')
+
 var Canvas = document.getElementById('canvas');
 var ctx = Canvas.getContext('2d');
 
-var resize = function() {
+var resize = function () {
     Canvas.width = Canvas.clientWidth;
     Canvas.height = Canvas.clientHeight;
 };
@@ -19,10 +26,10 @@ presets.o = function (x, y, s, dx, dy) {
         w: 5 * s,
         dx: dx,
         dy: dy,
-        draw: function(ctx, t) {
+        draw: function (ctx, t) {
             this.x += this.dx;
             this.y += this.dy;
-            
+
             ctx.beginPath();
             ctx.arc(this.x + + Math.sin((50 + x + (t / 10)) / 100) * 3, this.y + + Math.sin((45 + x + (t / 10)) / 100) * 4, this.r, 0, 2 * Math.PI, false);
             ctx.lineWidth = this.w;
@@ -43,13 +50,13 @@ presets.x = function (x, y, s, dx, dy, dr, r) {
         dx: dx,
         dy: dy,
         dr: dr,
-        draw: function(ctx, t) {
+        draw: function (ctx, t) {
             this.x += this.dx;
             this.y += this.dy;
             this.r += this.dr;
-            
+
             var _this = this;
-            var line = function(x, y, tx, ty, c, o) {
+            var line = function (x, y, tx, ty, c, o) {
                 o = o || 0;
                 ctx.beginPath();
                 ctx.moveTo(-o + ((_this.s / 2) * x), o + ((_this.s / 2) * y));
@@ -58,25 +65,25 @@ presets.x = function (x, y, s, dx, dy, dr, r) {
                 ctx.strokeStyle = c;
                 ctx.stroke();
             };
-            
+
             ctx.save();
-            
+
             ctx.translate(this.x + Math.sin((x + (t / 10)) / 100) * 5, this.y + Math.sin((10 + x + (t / 10)) / 100) * 2);
             ctx.rotate(this.r * Math.PI / 180);
-            
+
             line(-1, -1, 1, 1, '#FFEDBC');
             line(1, -1, -1, 1, '#FFEDBC');
-            
+
             ctx.restore();
         }
     }
 };
 
-for(var x = 0; x < Canvas.width; x++) {
-    for(var y = 0; y < Canvas.height; y++) {
-        if(Math.round(Math.random() * 8000) == 1) {
+for (var x = 0; x < Canvas.width; x++) {
+    for (var y = 0; y < Canvas.height; y++) {
+        if (Math.round(Math.random() * 8000) == 1) {
             var s = ((Math.random() * 5) + 1) / 10;
-            if(Math.round(Math.random()) == 1)
+            if (Math.round(Math.random()) == 1)
                 elements.push(presets.o(x, y, s, 0, 0));
             else
                 elements.push(presets.x(x, y, s, 0, 0, ((Math.random() * 3) - 1) / 10, (Math.random() * 360)));
@@ -84,13 +91,13 @@ for(var x = 0; x < Canvas.width; x++) {
     }
 }
 
-setInterval(function() {
-    ctx.clearRect(0, 0, Canvas.width, Canvas.height);
-
-    var time = new Date().getTime();
-    for (var e in elements)
-		elements[e].draw(ctx, time);
-}, 10);
+function Particle(params) {
+    setInterval(function () {
+        ctx.clearRect(0, 0, Canvas.width, Canvas.height);
+        for (var e in elements)
+            elements[e].draw(ctx, time.getTime());
+    }, 10);
+}
 
 function addZero(n) {
     return (parseInt(n, 10) < 10 ? '0' : '') + n;
@@ -100,20 +107,17 @@ function ClockTime() {
     var hours = document.getElementById('hours')
     var minutes = document.getElementById('minutes')
     var second = document.getElementById('ticker')
-    var time = new Date()
-    hours.innerHTML = addZero(time.getHours())
-    minutes.innerHTML = addZero(time.getMinutes())
-    second.innerHTML= addZero(time.getSeconds())
+    var date = document.getElementById('date')
+    hours.innerHTML = addZero(dayjs().hour())
+    minutes.innerHTML = addZero(dayjs().minute())
+    second.innerHTML = addZero(dayjs().second())
+    date.innerHTML = dayjs().locale('id').format('DD MMMM YYYY')
 
     setTimeout(ClockTime, 1000)
 }
 
-function greet() {
-    var androbar = document.getElementById('androbar')
-    var grettings = document.getElementById("grettings")
-    var jam = new Date().getHours()
-    var body = document.body
-
+function BgGreetingChange() {
+    Particle()
     if (jam < 12) {
         androbar.setAttribute('content', '#426fed')
         body.classList.add('grad1')
@@ -136,22 +140,38 @@ function greet() {
     }
 }
 
+function hbd() {
+    Confetti()
+    androbar.setAttribute('content', '#fc00ff')
+    body.classList.add('grad4')
+    grettings.innerHTML = "Selamat Ulang Tahun!!!"
+}
+
+function greet() {
+    var Hday = dayjs().locale('id').format('DD MMMM')
+    // var Hday = '30 Juli'
+    if (Hday !== '30 Juli') {
+        BgGreetingChange()
+    } else {
+        hbd()
+    }
+
+}
+
 var quotes = [
-        "Jangan lupa makan",
-        "Minum obat jangan lupa!",
-        "Mending inget tuhan",
-        "Sekolah yang bener!",
-        "Belajar dikit-dikit",
-        "Jangan pikun",
-        "Dah besar masih nangid?",
-        "Kalo malem tidur bego",
+    "Jangan lupa makan",
+    "Minum obat jangan lupa!",
+    "Mending inget tuhan",
+    "Sekolah yang bener!",
+    "Belajar dikit-dikit",
+    "Jangan pikun",
+    "Dah besar masih nangid?",
+    "Kalo malem tidur bego",
 ]
+quote.innerHTML = quotes[Math.floor(Math.random() * (7 + 1))]
 
 ClockTime()
-greet()
-document.getElementById('quote').innerHTML = quotes[Math.floor(Math.random() * (7 + 1))]
-
 setInterval(greet(), 10000)
 setInterval(() => {
-    document.getElementById('quote').innerHTML = quotes[Math.floor(Math.random() * (7 + 1))]
+    quote.innerHTML = quotes[Math.floor(Math.random() * (7 + 1))]
 }, 5000)
